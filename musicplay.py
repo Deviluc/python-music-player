5553d077412e5e51176d533099e62536bbb05c4e
lucas <lucas@BB-PC>
2015-08-20 20:16:49 +0200

    Fixed Programm

--- musicplay.py
+++ musicplay.py
@@ -6,18 +6,18 @@ user_input = raw_input("Enter the path of your file: ")
 MusicD = user_input
 x = os.listdir(MusicD)
 
+mixer.init()
+
 for i in range (0, len(x)):
 	#print x[i]
 	matchObject = re.search('.*\.mp3', x[i])
 
 	if matchObject:
 		print "Found an mp3 file: " + x[i]
+		print "Playing it now..."
+		mixer.music.load(MusicD + x[i])
+		mixer.music.play()
 
- 
-mixer.init()
- 
-mixer.music.load("filepath.mp3")
-mixer.music.play()
  
  
 while mixer.music.get_busy():
